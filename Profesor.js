03eee24ac072d1d3fbd6a09a95cde5da118d4e53
santi-dev1 <santirojass2004@gmail.com>
2024-04-12 21:14:42 -0300

    Reforma estructura del programa

--- Profesor.js
+++
@@ -1,9 +0,0 @@
-import { Empleado } from "./Empleado";
-
-class Profesor extends Empleado {
-    constructor(nombre, apellidos, numeroIdentificacion, estadoCivil, anoIncorporacion, numeroDespacho, departamento){
-        super(nombre, apellidos, numeroIdentificacion, estadoCivil, anoIncorporacion, numeroDespacho);
-
-        this.departamento = departamento;
-    }
-}
